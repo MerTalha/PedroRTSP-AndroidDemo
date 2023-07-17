c730572bc37c21f987720e38b5d07c14a2f6008e
Mert Talha <merttalhasimsek@gmail.com>
2023-07-17 16:14:30 +0300

    Update VideoStreamPlayer.java

--- app/src/main/java/com/example/pedrortsp_androiddemo/VideoStreamPlayer.java
+++ app/src/main/java/com/example/pedrortsp_androiddemo/VideoStreamPlayer.java
@@ -10,7 +10,7 @@ import org.videolan.libvlc.MediaPlayer;
 import org.videolan.libvlc.util.VLCVideoLayout;
 
 public class VideoStreamPlayer extends Activity {
-    private  LibVLC libVlc;
+    private final LibVLC libVlc;
     private final MediaPlayer mediaPlayer;
     private final VLCVideoLayout videoLayout;
 
@@ -25,62 +25,17 @@ public class VideoStreamPlayer extends Activity {
     {
         mediaPlayer.attachViews(videoLayout, null, false, false);
 
-        /*ArrayList<String> options = new ArrayList<String>();
-        options.add("--no-drop-late-frames");
-        options.add("--no-skip-frames");
-        options.add("--rtsp-tcp");
-        options.add("-vvv");*/
-
         Media media = new Media(libVlc, Uri.parse(url));
         media.setHWDecoderEnabled(true, false);
         media.addOption(":skip-frames");
         media.addOption(":codec=mediacodec,iomx,all");
         media.addOption(":vcodec=h264");
-        media.addOption(":network-caching=105");
+        media.addOption(":rtsp-tcp");
+        media.addOption(":network-caching=110");
         media.addOption(":clock-jitter=0");
         media.addOption(":clock-synchro=0");
-        media.addOption(":clock-quantization=1");
+        media.addOption(":clock-quantization=0");
 
-        /*for (String option : options) {
-            media.addOption(option);
-        }*/
-        /*media.addOption(":file-caching=130");
-        media.addOption(":live-caching=130");
-        media.addOption(":sout-mux-caching=130");*/
-
-
-        /*
-        ArrayList<String> options = new ArrayList<String>();
-        options.add("--aout=opensles");
-        options.add("--audio-time-stretch"); // time stretching
-        options.add("-vvv"); // verbosity
-        libVlc = new LibVLC(MainActivity.this, options);
-        int cache = 1500;
-        1-
-        media.addOption("--aout=opensles");
-        2-
-        media.addOption("--audio-time-stretch"); // time stretching
-        3-
-        media.addOption("-vvv"); // verbosity
-        4-
-        media.addOption("--aout=opensles");
-        5-
-        media.addOption("--avcodec-codec=h264");
-        6-
-        media.addOption("--file-logging");
-        7-
-        media.addOption("--logfile=vlc-log.txt");
-        8-
-        media.addOption(":network-caching=" + cache);
-        9-
-        media.addOption(":file-caching=" + cache);
-        10-
-        media.addOption(":live-cacheing=" + cache);
-        11-
-        media.addOption(":sout-mux-caching=" + cache);
-        12-
-        media.addOption(":codec=mediacodec,iomx,all");
-        */
         mediaPlayer.setMedia(media);
         media.release();
         mediaPlayer.play();
